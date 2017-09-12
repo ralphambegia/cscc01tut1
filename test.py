fc16c5f3196090a58e111b51df786bbd88fda17d
Jacob Roland Chamberlain <chamb139@IITS-B473-23.utsc-labs.utoronto.ca>
2017-09-12 11:30:43 -0400

    added random boolean generator

--- test.py
+++ test.py
@@ -3,3 +3,11 @@ y = 2
 z = 3
 a = x + z + y
 print(a)
+
+def genRandBool():
+	seed = ["eni", "menie", "minie", "moe", "catch", "a", "tiger", "by", "the", "toe"]
+	bool retn = True
+	for i in range(0, len(seed)):
+		retn = !retn
+	return retn 
+	
